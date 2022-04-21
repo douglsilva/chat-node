9a207321ae1ceb6843fc7c5b637be623e6afb362
Douglas da Silva <douglasdasilva448@gmail.com>
2022-04-21 11:30:27 -0300

    Configura socket.io para funcionar no servidor

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const path = require('path');
 const http = require('http');
+const socketIO = require('socket.io');
 
 const app = express();
-const server = http.createServer(app)
+const server = http.createServer(app);
+const io = socketIO(server);
 
 server.listen(3000);
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Listener de conexão
+io.on('connection', (socket) => {
+  console.log('Conexão detectada...');
+});
